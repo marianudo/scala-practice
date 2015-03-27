88a9eaad25e319e79c6d146fe48fc32565304814
Mariano Navas <marianonavas@packlink.es>
2015-03-27 16:04:14 +0100

    Tail recursive implementation of zip

--- src/main/scala/com/codinginflipflops/learn/scala/ListFunctions.scala
+++ src/main/scala/com/codinginflipflops/learn/scala/ListFunctions.scala
@@ -19,25 +19,30 @@ object ListFunctions {
       if (growingList.length == numberOfElements) growingList
       else aux(sourceList.tail, growingList ++ List(sourceList.head))
     }
-    if(ls.isEmpty) return Nil
-    if(ls.length < numberOfElements) ls
+    if (ls.isEmpty) return Nil
+    if (ls.length < numberOfElements) ls
     else aux(ls, Nil)
   }
 
   def drop(ls: List[Int], numberOfElements: Int): List[Int] = {
-    if(numberOfElements == 0) ls
+    if (numberOfElements == 0) ls
     else drop(ls.tail, numberOfElements - 1)
   }
 
   def zip(l1: List[Int], l2: List[Int]): List[(Int, Int)] = {
-    if(l1.isEmpty || l2.isEmpty) Nil
-    else (l1.head, l2.head) :: zip(l1.tail, l2.tail)
+    //    if(l1.isEmpty || l2.isEmpty) Nil
+    //    else (l1.head, l2.head) :: zip(l1.tail, l2.tail)
+    def aux(l1: List[Int], l2: List[Int], output: List[(Int, Int)]): List[(Int, Int)] = {
+      if(l1.isEmpty || l2.isEmpty) output
+      else aux(l1.tail, l2.tail, output ::: List((l1.head, l2.head)))
+    }
+    aux(l1, l2, Nil)
   }
 
   def reverse(ls: List[Int]): List[Int] = {
     def aux(sourceList: List[Int], outputList: List[Int]): List[Int] = {
-      if(sourceList.isEmpty) outputList
-      else aux(take(sourceList, sourceList.length - 1), outputList ::: List(last(sourceList) ))
+      if (sourceList.isEmpty) outputList
+      else aux(take(sourceList, sourceList.length - 1), outputList ::: List(last(sourceList)))
     }
     aux(ls, Nil)
   }
